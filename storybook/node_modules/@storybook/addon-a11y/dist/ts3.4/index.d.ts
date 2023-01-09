a9931cc3b45d1e5ce0053cd22d6e4df8b7b7e58d
Joseph Eccles <joseph.eccles@gmail.com>
2023-01-09 11:08:55 +0000

    remove node modules

--- storybook/node_modules/@storybook/addon-a11y/dist/ts3.4/index.d.ts
+++
@@ -1,5 +0,0 @@
-import { AnyFramework, DecoratorFunction } from '@storybook/csf';
-export { PARAM_KEY } from './constants';
-export * from './highlight';
-export * from './params';
-export declare const withA11y: DecoratorFunction<AnyFramework>;
--- storybook/node_modules/@storybook/addon-a11y/dist/ts3.9/components/Report/Item.d.ts
+++
@@ -1,8 +0,0 @@
-import { Result } from 'axe-core';
-import { RuleType } from '../A11YPanel';
-interface ItemProps {
-    item: Result;
-    type: RuleType;
-}
-export declare const Item: (props: ItemProps) => JSX.Element;
-export {};
